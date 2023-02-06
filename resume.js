72754cab71b2d2c5d8aa3a1662d2992301f5af79
NonnieNguyen <alannguyen514@gmail.com>
2023-02-06 13:38:45 -0500

    work on css

---
+++ resume.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            
+            entry.target.classList.add('show');
+            // entry.target.classList.remove('hidden');
+        }
+    })
+});
+
+for(const elem of document.getElementsByClassName('hidden')){
+    observer.observe(elem);
+
+}
